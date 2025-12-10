9b233d10186045dae8b007a42e413c81a6b77d7c
Rouki1210 <115490379+Rouki1210@users.noreply.github.com>
2025-12-10 16:23:23 +0700

    Update googleRedirectUri to remove auth callback path

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     googleClientId: '461833316536-2silvjehavg5u5kjlcb0f9hebbiqlesl.apps.googleusercontent.com',
     apiBaseUrl: 'https://localhost:7175',
-    googleRedirectUri: 'https://market-analyst-front-end.vercel.app/auth/callback/google' 
+    googleRedirectUri: 'https://market-analyst-front-end.vercel.app' 
 };
