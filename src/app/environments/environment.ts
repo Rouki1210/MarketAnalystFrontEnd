55c627998543a65c94c30ba474995e6313224195
Rouki1210 <115490379+Rouki1210@users.noreply.github.com>
2025-12-10 14:00:15 +0700

    new update

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    googleClientId: '461833316536-2silvjehavg5u5kjlcb0f9hebbiqlesl.apps.googleusercontent.com',
+    apiBaseUrl: 'https://localhost:7175',
+    googleRedirectUri: 'http://localhost:4200' 
+};
