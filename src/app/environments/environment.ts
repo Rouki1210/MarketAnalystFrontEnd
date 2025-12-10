445ce4b82101316280e852cbb5301a88e856e1d0
Rouki1210 <115490379+Rouki1210@users.noreply.github.com>
2025-12-10 16:16:21 +0700

    Update googleRedirectUri to production URL

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     googleClientId: '461833316536-2silvjehavg5u5kjlcb0f9hebbiqlesl.apps.googleusercontent.com',
     apiBaseUrl: 'https://localhost:7175',
-    googleRedirectUri: 'http://localhost:4200' 
+    googleRedirectUri: 'https://market-analyst-front-end.vercel.app/auth/callback/google' 
 };
